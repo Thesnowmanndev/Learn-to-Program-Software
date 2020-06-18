a99b99e690d79452e9e99f2a4f9e05451e2ccc63
Kyle Martin <kyle.martin.0819@gmail.com>
2020-06-18 09:50:38 -0400

    Create StringsAreArrays.py

---
+++ Learn Python/Basic Data Structures/StringsAreArrays.py
@@ -0,0 +1,12 @@
+# Welcome to Learn Python created by Kyle Martin | https://github.com/Thesnowmanndev This repository is mainly for my
+# notes while learning how to program in python. Feel free to read over the files in this repository and learn what
+# you can. I will try to leave as detailed notes in a comment form as I can but if you are new to programming dont
+# get in the habit of making 100s of comments. Strive to have clean readable code that doesnt require comments to
+# explain what it does. That will make you a better developer in the long run.
+
+# IMPORTANT: This is a more advanced document of Strings in Python. I recommend you look over /Basics/SimpleStrings.py
+# first before you learn this file.
+
+# There is a lot of handy things you can do with strings in Python. For starters all strings are arrays. What is an
+# array? An array is simply a list of data. An array is also known as a data structure (a structure that holds some
+# data together).
