21414c739097434ddf9ac888315d98e5815d3a18
Kyle Martin <kyle.martin.0819@gmail.com>
2020-06-23 21:26:40 -0400

    Create 13_SequenceTypes.py

---
+++ Learn Python/Basics/13_SequenceTypes.py
@@ -0,0 +1,24 @@
+# Welcome to Learn Python created by Kyle Martin | https://github.com/Thesnowmanndev | This repository is mainly for my
+# notes while learning how to program in python. Feel free to read over the files in this repository and learn what
+# you can. I will try to leave as detailed notes in a comment form as I can but if you are new to programming dont
+# get in the habit of making 100s of comments. Strive to have clean readable code that doesnt require comments to
+# explain what it does. That will make you a better developer in the long run.
+
+# Sequence Types in Python:
+# Python has 5 sequence types built in. They are The string, list, tuple, range, and byte / bytearrays
+# A sequence is an ordered set of items. For example:
+
+greeting = "Hello World!"
+
+# The string above is a sequence. It is an ordered set of items each being the characters in the string. 
+# A list is also a sequence type:
+
+equipment_needed = ["computer", "monitor", "keyboard", "mouse", "cords"]
+
+# The list above is a sequence type, it contains 5 items each being a string. 
+
+
+
+if __name__ == '__main__':
+    print(equipment_needed)
+    
